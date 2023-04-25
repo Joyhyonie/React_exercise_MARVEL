3b5eb957458a61c5c78b830052ffb89b30b0c95c
Joyhyonie <cho970129@gmail.com>
2023-04-25 23:53:47 +0900

    ✨ Feat: to-top button 추가

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,25 @@ function Navbar() {
 
     const linkStyle = { textDecoration: "none" }
 
+    const handleScrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
     return (
-        <div className={ customStyle.Navbar } >
-            <Link to="characters" style={ linkStyle }><div>CHARACTERS</div></Link>
-            <Link to="comics" style={ linkStyle }><div>COMICS</div></Link>
-        </div>
+        <>
+            <div className={ customStyle.Navbar } >
+                <Link to="characters" style={ linkStyle }><div>CHARACTERS</div></Link>
+                <Link to="comics" style={ linkStyle }><div>COMICS</div></Link>
+            </div>
+            <img 
+                src="/images/totop.png" 
+                className={ customStyle.ToTop } 
+                onClick={ handleScrollToTop }
+            />
+        </>
     );
 }
 
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,11 +2,12 @@ import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from 'react-router-dom';
+import customStyle from '../css/Common.module.css';
 
 function Layout() {
 
     return (
-        <>
+        <>  
             <Header/>
             <Navbar/>
             <Outlet/>
